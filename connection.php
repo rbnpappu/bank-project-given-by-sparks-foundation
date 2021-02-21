2d024bec43fdd6b9ca6813221aaa02b94180ddb5
rbnpappu <52997903+rbnpappu@users.noreply.github.com>
2021-02-21 10:20:14 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bankdatabase";
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+/*
+if($conn)
+{
+	echo "Connection Succesfull";
+}
+else
+{
+	echo "Connection failed".mysqli_connect_error();
+}
+*/
+?>
